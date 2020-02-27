9ad4899f914fb4a85fee650b3d4881a6cd62701e
Zoe Christ <zoe@christ.de>
2020-02-27 15:17:05 +0100

    view 2

---
+++ app/src/main/java/com/example/mobileapplikationen/filter_1.java
@@ -0,0 +1,14 @@
+package com.example.mobileapplikationen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class filter_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter_1);
+    }
+}
